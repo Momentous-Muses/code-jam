0666efdaa2124912a5b4af6e01231806804a0f86
TobeTek <katchyemma@gmail.com>
2022-07-23 16:41:55 +0100

    fix and lint

--- poc/ws_client.py
+++ poc/ws_client.py
@@ -1,44 +1,49 @@
-#TODO: Add Exception Handling for websocket disconnect
-from dataclasses import dataclass
-import typing
-from websockets import connect 
-from datetime import datetime 
-import asgiref.sync
+# TODO: Add Exception Handling for websocket disconnect
 import asyncio
+import typing
+from dataclasses import dataclass
+from datetime import datetime
+
+import asgiref.sync
+from websockets import connect
 
 
 class Event:
-    pass 
+    pass
+
 
 @dataclass
 class NewMessage(Event):
-    """ 
+    """
     Event that gets emitted whenever a new message is created
     Should be handled my the EventHandler
     """
-    client_id: str 
+
+    client_id: str
     room_id: str
     data: typing.Any
-    
+
     type: str = "new-message"
-    
 
     @classmethod
     def from_json(cls, data):
         return cls(**data)
 
+
 msg_data = {
-    "timestamp": datetime.now().isoformat(),# ISO format time
+    "timestamp": datetime.now().isoformat(),  # ISO format time
     "content": "Message content",
 }
-msg = NewMessage.from_json({"client_id":"client_Peter0912", "room_id":"room_9023as", "data":msg_data})
+msg = NewMessage.from_json(
+    {"client_id": "client_Peter0912", "room_id": "room_9023as", "data": msg_data}
+)
 print(msg)
 
 
 class ClientConnection:
     def __init__(self, ws_connection, *args, **kwargs):
         self.ws_connection = ws_connection
-    
+
     @classmethod
     async def start_connection(cls, uri, *args, **kwargs):
         connection = await connect(uri)
@@ -49,7 +54,7 @@ class ClientConnection:
         # Reading stdin is blocking
         txt = input(">>> ")
         return txt
-    
+
     async def consumer(self, msg):
         # Do something with the received msg
         print(msg)
@@ -69,18 +74,18 @@ class ClientConnection:
             self.consumer_handler(self.ws_connection),
             self.producer_handler(self.ws_connection),
         )
-    
-    
+
+
 URI = "wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self"
 URI2 = "ws://localhost:8000/ws/20"
 
+
 async def main():
     client = await ClientConnection.start_connection(URI2)
-    
+
     # listen for blocking input in the background
     asyncio.create_task(client.producer())
     await client.handler()
-    
+
+
 asyncio.run(main())
-
-
