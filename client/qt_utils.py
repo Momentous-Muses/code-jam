0ff3119e31d3a118c3beeec316e1e2ede1b21053
Numerlor <Numerlor@gmail.com>
2022-07-26 22:38:39 +0200

    add interrupt timer, rename to qt utils

--- client/qt_utils.py
+++ client/qt_utils.py
@@ -22,3 +22,12 @@ def init_qt_logging() -> None:
         qt_logger.log(QT_LOG_LEVELS[level], message)
 
     QtCore.qInstallMessageHandler(handler)
+
+
+def interrupt_timer(app: QtCore.QCoreApplication) -> QtCore.QTimer:
+    """Interrupt the Qt event loop regularly to let python process keyboard interrupts."""
+    timer = QtCore.QTimer(app)
+    timer.interval = 200
+    timer.timeout.connect(lambda: None)
+    timer.start()
+    return timer
