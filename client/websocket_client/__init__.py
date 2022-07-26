ed8d6622f5866d0fe2c61d1bf74531f0ec62f539
Numerlor <25886452+Numerlor@users.noreply.github.com>
2022-07-26 15:52:11 +0200

    use tuple for __all__ instead of list
    Co-authored-by: Oliver Wilkes <oliverwilkes2006@icloud.com>

--- client/websocket_client/__init__.py
+++ client/websocket_client/__init__.py
@@ -2,4 +2,4 @@ from .client import WebsocketMessageDispatcher
 from .messages import Message
 from .utils import UNINITIALIZED, ResultEvent
 
-__all__ = ["Message", "ResultEvent", "UNINITIALIZED", "WebsocketMessageDispatcher"]
+__all__ = ("Message", "ResultEvent", "UNINITIALIZED", "WebsocketMessageDispatcher")
