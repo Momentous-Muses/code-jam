00667225984cccd2499ad514c926795e46782e0c
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2022-07-28 00:42:52 +0100

    Create template for server's rooms.py
    This module provides classes for creating and managing rooms on the
    server.

---
+++ server/rooms.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""Module to create manage rooms."""
+
+
+class Room:
+    """A single room of users.
+
+    This class should contain the necessary functions to manage a single room, such as connecting/disconnecting users,
+      adding/removing apps/plugins, etc.
+    """
+
+    pass
+
+
+class RoomManager:
+    """Store/Manage Rooms.
+
+    This class will store and manage all the rooms on an instance of the server.
+    """
+
+    pass
