a7f3685a16ddc0bffa23831fb11f76ac914f4457
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2022-07-31 13:09:07 +0100

    Create Plugin Module.
    Note that all additional plugins should be kept inside the plugins
    module, at the same level as __init__.py.

---
+++ server/plugins/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+"""Plugin Module.
+
+This will include the base class for plugins on the server.
+"""
+
+
+class BasePlugin:
+    """Base Plugin Class.
+
+    All plugins on the server should inherit from this class.
+    """
+
+    pass
